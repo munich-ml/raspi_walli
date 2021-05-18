e701c0d590a7ba2277b206ea3fa79f2a8e8da629
Holger <holger.steffens@gmail.com>
2021-05-18 20:30:53 +0200

    pymodbus on Python 3.5

--- modbus_trial.py
+++ modbus_trial.py
@@ -1,4 +1,9 @@
+import serial
 from pymodbus.client.sync import ModbusSerialClient
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
 
 walli = ModbusSerialClient(method="rtu",
                            port="/dev/ttyAMA0",
@@ -9,6 +14,6 @@ walli = ModbusSerialClient(method="rtu",
 
 walli.connect()
 
-r = walli.read_input_registers()
+r = walli.read_input_registers(13, 1)
 
 print(r)
