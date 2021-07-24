a562042dc8d03c6ba12e4af4c01a1aee9ed2f0e8
Holger <holger.steffens@gmail.com>
2021-07-24 10:20:42 +0200

    Update modbus_trial.py

--- modbus_trial.py
+++ modbus_trial.py
@@ -1,19 +1,33 @@
-import serial
 from pymodbus.client.sync import ModbusSerialClient
+"""
+import serial
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
+"""
 
 walli = ModbusSerialClient(method="rtu",
-                           port="/dev/ttyAMA0",
+                           port='/dev/ttyUSB0',
                            baudrate=19200,
                            stopbits=1,
                            bytesize=8,
-                           parity="E")
+                           parity="E",
+                           timeout=10)
 
 walli.connect()
 
-r = walli.read_input_registers(13, 1)
+while True:
+    try:
+        r = walli.read_input_registers(4, count=15, unit=1)
+        if r.isError():
+            print("Error")
+        else:
+            print(r.registers)
 
-print(r)
+    except KeyboardInterrupt as e:
+        print(e)
+        break
+
+walli.close()
+
