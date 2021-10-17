8ac83bc4a911446428ab08a35f95a33880cb9ce6
Holger <holger.steffens@gmail.com>
2021-10-17 09:35:08 +0200

    relationship

--- database/database_app.py
+++ database/database_app.py
@@ -5,15 +5,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-class WalliStat(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    datetime = db.Column(db.DateTime, nullable=False, unique=True)
-    Temp = db.Column(db.Float, nullable=False, unique=False)
-    Power = db.Column(db.Integer, nullable=False, unique=False)
-    
-    def __repr__(self):
-        return f"WalliStat(id:{self.id}, {self.datetime}: {self.Temp}°C, {self.Power}W)"
-
 class Campaign(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False, unique=False)
@@ -23,6 +14,18 @@ class Campaign(db.Model):
     interval = db.Column(db.Interval, nullable=False, unique=False)
     measure_walli = db.Column(db.Boolean, default=True)
     measure_light = db.Column(db.Boolean, default=True)
+    walli_stats = db.relationship('WalliStat', backref='campaign', lazy=True)
 
     def __repr__(self):
         return f"Campaign(id:{self.id}, '{self.title}' is active:{self.is_active}, start:{self.start}, end:{self.end}, interval:{self.interval})"
+
+
+class WalliStat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime, nullable=False, unique=True)
+    Temp = db.Column(db.Float, nullable=False, unique=False)
+    Power = db.Column(db.Integer, nullable=False, unique=False)
+    campaign_id = db.Column(db.Integer, db.ForeignKey('campaign.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"WalliStat(id:{self.id}-->campaign.id:{self.campaign_id}, {self.datetime}: {self.Temp}°C, {self.Power}W)"
