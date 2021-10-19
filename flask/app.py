40bf7e60ed92ffc3967e2767a7db913e92c6719e
Holger <holger.steffens@gmail.com>
2021-10-19 21:19:44 +0200

    campaign rendering implemented

--- flask/app.py
+++ flask/app.py
@@ -3,9 +3,38 @@
 
 import os, json
 import pandas as pd
-from flask import Flask, render_template, flash, redirect, url_for, request, send_from_directory, session
+from flask import Flask, render_template, flash, redirect, url_for, request, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trial.db'
+db = SQLAlchemy(app)
+
+class Campaign(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    start = db.Column(db.DateTime, nullable=False)
+    end = db.Column(db.DateTime)
+    interval = db.Column(db.Interval, nullable=False)
+    measure_walli = db.Column(db.Boolean, default=True)
+    measure_light = db.Column(db.Boolean, default=True)
+    walli_stats = db.relationship('WalliStat', backref='campaign', lazy=True)
+
+    def __repr__(self):
+        return f"Campaign(id:{self.id}, '{self.title}' is active:{self.is_active}, start:{self.start}, end:{self.end}, interval:{self.interval})"
+
+
+class WalliStat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime)
+    Temp = db.Column(db.Float)
+    Power = db.Column(db.Integer)
+    campaign_id = db.Column(db.Integer, db.ForeignKey('campaign.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"WalliStat(id:{self.id}-->campaign.id:{self.campaign_id}, {self.datetime}: {self.Temp}°C, {self.Power}W)"
+
 
 @app.route('/')
 def index():
@@ -13,7 +42,12 @@ def index():
 
 @app.route('/history/')
 def history():
-    return render_template('history.html')
+    campaigns = Campaign.query.all()
+    for campaign in campaigns:
+        print(campaign)
+
+    return render_template('history.html', campaigns=campaigns)
+
 
 @app.route('/config/')
 def config():
