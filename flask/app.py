598177195edecd2b3931224759f196310795599c
Holger <holger.steffens@gmail.com>
2021-10-24 19:47:06 +0200

    BooleanFields added

--- flask/app.py
+++ flask/app.py
@@ -5,11 +5,11 @@ import os, json
 from numpy import tile
 import datetime as dt
 import pandas as pd
-from flask import Flask, render_template, flash, redirect, request, session
+from flask import Flask, render_template, flash, redirect, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, TextAreaField
-from wtforms.fields.html5 import DateField, DateTimeField, TimeField
+from wtforms import StringField, TextField, TextAreaField, BooleanField
+from wtforms.fields.html5 import IntegerField, DateField, TimeField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -32,6 +32,30 @@ class Campaign(db.Model):
         return f"Campaign(id:{self.id}, '{self.title}' is active:{self.is_active}, start:{self.start}, end:{self.end}, interval:{self.interval})"
 
 
+class CampaignForm(FlaskForm):
+    id = IntegerField(render_kw={'readonly': True})
+    title = TextAreaField(validators=[DataRequired()])
+    active = BooleanField(default=True)
+    start_date = DateField(format="%Y-%m-%d", validators=[DataRequired()], default=dt.datetime.now().date())
+    start_time = TimeField(format="%H:%M", validators=[DataRequired()], default=dt.datetime.now().time())
+    end_date = DateField(format="%Y-%m-%d", validators=[DataRequired()], default=dt.datetime.now().date())
+    end_time = TimeField(format="%H:%M", validators=[DataRequired()], default=dt.datetime.now().time())
+    measure_walli = BooleanField(default=True)
+    measure_light = BooleanField(default=True)
+        
+    def populate(self, cpn):
+        self.id.data = cpn.id
+        self.title.data = cpn.title
+        self.active.data = cpn.is_active
+        self.start_date.data = cpn.start.date()
+        self.start_time.data = cpn.start.time()
+        self.end_date.data = cpn.end.date()
+        self.end_time.data = cpn.end.time()
+        self.measure_walli.data = cpn.measure_walli
+        self.measure_light.data = cpn.measure_light
+
+        
+
 class WalliStat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     datetime = db.Column(db.DateTime)
@@ -43,19 +67,6 @@ class WalliStat(db.Model):
         return f"WalliStat(id:{self.id}-->campaign.id:{self.campaign_id}, {self.datetime}: {self.Temp}°C, {self.Power}W)"
 
 
-class CampaignForm(FlaskForm):
-    title = TextAreaField(validators=[DataRequired()])
-    start_date = DateField(format="%Y-%m-%d", validators=[DataRequired()], default=dt.datetime.now().date())
-    start_time = TimeField(format="%H:%M", validators=[DataRequired()], default=dt.datetime.now().time())
-    end_date = DateField(format="%Y-%m-%d", validators=[DataRequired()], default=dt.datetime.now().date())
-    end_time = TimeField(format="%H:%M", validators=[DataRequired()], default=dt.datetime.now().time())
-
-    def populate(self, cpn):
-        self.title.data = cpn.title
-        self.start_date.data = cpn.start.date()
-        self.start_time.data = cpn.start.time()
-        self.end_date.data = cpn.end.date()
-        self.end_time.data = cpn.end.time()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -85,20 +96,25 @@ def history():
 
 @app.route('/history/edit/', methods=['GET', 'POST'], defaults={"id": None})
 @app.route('/history/edit/<id>/', methods=['GET', 'POST'])
-def edit(id=None):
-    form = CampaignForm()    
-    if form.validate_on_submit():    
-        print("in modify, method=", request.method)
+def edit(id=None):   
+    if request.method == "GET": 
+        form = CampaignForm()    
+        if id is not None:
+            cpn = Campaign.query.get(id)
+            form.populate(cpn)
+        return render_template('edit.html', form=form)    
+    
+    elif request.method == "POST":    
+        print("### edit POST ###")
+        print("method:", request.method, ", todo:", request.form["todo"])
         for key, value in request.form.items():
-            print(key, value, type(value))
+            print("-", key, value, type(value))
 
         return redirect('/history/')
 
-    if id is not None:
-        cpn = Campaign.query.get(id)
-        form.populate(cpn)
+    else:
+        print(f"Warning: Unsupported request.method '{request.method}'!")
 
-    return render_template('edit.html', form=form)
 
 
 if __name__ == "__main__":
