80f026b9723aac078b832cef4405bc60636ce0d2
Holger <holger.steffens@gmail.com>
2022-01-09 17:03:27 +0100

    ajax DataTable with campaign_id filter

--- flask/app.py
+++ flask/app.py
@@ -495,15 +495,21 @@ def edit(id=None):
         logger.warning(f"Unsupported '{request.method=}'!")
 
 
-@app.route('/data/')
-def data():
+@app.route('/data/', methods=['GET'], defaults={"campaign_id": None})
+@app.route('/data/<campaign_id>/', methods=['GET'])
+def data(campaign_id=None):
     """ View function for data page """
-    return render_template('data.html', title="Holger")
+    if campaign_id is None:
+        campaign_id = 0
+        
+    return render_template('data.html', campaign_id=campaign_id)
 
 
-@app.route('/api/data')
-def api_data():
-    return {'data': [lv.to_dict() for lv in LuxValue.query]}
+@app.route('/api/data/light/<campaign_id>/', methods=['GET'])
+def api_get_light_data(campaign_id):
+    """provides sensor data for ajax DataTable"""
+    query = db.session.query(LuxValue).filter(LuxValue.campaign_id==campaign_id)
+    return {'data': [item.to_dict() for item in query]}
 
 
     
